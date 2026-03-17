a2f7169785dee07a662efc33c3ece6dbb7e27691
Darsshinee <ds0127@srmist.edu.in>
2026-03-17 06:01:41 +0530

    Printing oops banner using inline array intialization and for each loop

--- OOPSBanner-App.java
+++ OOPSBanner-App.java
@@ -1,15 +1,15 @@
 public OOPSBanner-App  {
     public static void main(String[] args) {
-		String[] lines = new String[7];
+		String[] lines = {
 
-        lines[0] = System.out.println(String.join("  ***   ", "  ", "  ***   ", "  ", "  *****  ", "  ", "  ******  "));
-        lines[1] = System.out.println(String.join(" **  ** ", "  ", " **  ** ", "  ", "  **  ** ", "  ", "  **       "));
-        lines[2] = System.out.println(String.join("**    **", "  ", "**    **", "  ", "  **  ** ", "  ", "  **     "));
-        lines[3] = System.out.println(String.join("**    **", "  ", "**    **", "  ", "  ****** ", "  ", "  ******  "));
-        lines[4] = System.out.println(String.join("**    **", "  ", "**    **", "  ", "  **     ", "  ", "      ** "));
-        lines[5] = System.out.println(String.join(" **  ** ", "  ", " **  ** ", "  ", "  **     ", "  ", "      ** "));
-        lines[6] = System.out.println(String.join("  ***   ", "  ", "  ***   ", "  ", "  **     ", "  ", "  ******  "));
-		
+        (String.join("   ***   ", "  ", "   ***   ", "  ", "  *****  ", "  ", "  ******  "),
+        (String.join(" **   ** ", "  ", " **   ** ", "  ", "  **  ** ", "  ", "  **       "),
+        (String.join("**     **", "  ", "**     **", "  ", "  **  ** ", "  ", "  **     "),
+        (String.join("**     **", "  ", "**     **", "  ", "  ****** ", "  ", "  ******  "),
+        (String.join("**     **", "  ", "**     **", "  ", "  **     ", "  ", "      ** "),
+        (String.join(" **   ** ", "  ", " **   ** ", "  ", "  **     ", "  ", "      ** "),
+        (String.join("   ***   ", "  ", "   ***   ", "  ", "  **     ", "  ", "  ******  ")
+		};
 		for (String line : lines){
 			System.out.println(line);
 
